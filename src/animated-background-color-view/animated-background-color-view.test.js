c9d23d52d9767d8152524f7a1074aac5e6e4ef64
Joey Schroeder <schrjos@gmail.com>
2018-10-22 07:14:21 -0500

    Updated test coverage

--- src/animated-background-color-view/animated-background-color-view.test.js
+++ src/animated-background-color-view/animated-background-color-view.test.js
@@ -1,8 +1,33 @@
 import { AnimatedBackgroundColorView } from './animated-background-color-view.component';
 import React from 'react';
 import { create } from 'react-test-renderer';
+import { shallow } from 'enzyme';
 
 describe('<AnimatedBackgroundColorView />', () => {
+  describe('this.componentDidMount', () => {
+    const component = shallow(<AnimatedBackgroundColorView />);
+    const animate = jest.fn();
+
+    component.instance().animate = animate;
+    component.instance().componentDidMount();
+
+    it('should call this.animate when called', () => {
+      expect(animate).toHaveBeenCalled();
+    });
+  });
+
+  describe('this.componentDidUpdate', () => {
+    const component = shallow(<AnimatedBackgroundColorView />);
+    const animate = jest.fn();
+
+    component.instance().animate = animate;
+    component.instance().componentDidUpdate();
+
+    it('should call this.animate when called', () => {
+      expect(animate).toHaveBeenCalled();
+    });
+  });
+
   it('should render correctly', () => {
     const component = create(<AnimatedBackgroundColorView />).toJSON();
     expect(component).toMatchSnapshot();
