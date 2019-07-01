3209fbd9da855b274a94d0636c284ec0a886ad8c
Joey Schroeder <schrjos@gmail.com>
2019-06-30 23:13:51 -0500

    Upgrade to Babel 7

---
+++ babel.config.js
@@ -0,0 +1,8 @@
+module.exports = api => {
+  api.cache(true);
+
+  return {
+    presets: ['@babel/preset-env', 'module:metro-react-native-babel-preset'],
+    plugins: ['@babel/plugin-proposal-class-properties']
+  };
+};
