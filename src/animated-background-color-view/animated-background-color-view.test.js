0d6364cf0b7edb16cf6fb8b59ce45c2ef34ce5ec
Joey Schroeder <schrjos@gmail.com>
2019-07-01 18:39:22 -0500

    Add test coverage

--- src/animated-background-color-view/animated-background-color-view.test.js
+++ src/animated-background-color-view/animated-background-color-view.test.js
@@ -1,4 +1,5 @@
 import { AnimatedBackgroundColorView } from './animated-background-color-view.component';
+import Color from 'color';
 import React from 'react';
 import { View } from 'react-native';
 import { create } from 'react-test-renderer';
@@ -28,7 +29,7 @@ describe('<AnimatedBackgroundColorView />', () => {
       expect(result).toEqual(true);
     });
 
-    it('should return fakse when this.props.children or this.props.color have not changed', () => {
+    it('should return false when this.props.children or this.props.color have not changed', () => {
       const nextProps = { children: <View />, color: 'test-1' };
       const component = shallow(
         <AnimatedBackgroundColorView color={nextProps.color}>{nextProps.children}</AnimatedBackgroundColorView>
@@ -48,6 +49,31 @@ describe('<AnimatedBackgroundColorView />', () => {
 
     it('should call this.animate', () => {
       expect(animate).toHaveBeenCalled();
+    });
+  });
+
+  describe('this.getCurrentColor', () => {
+    it('should return this.props.color when animationFinished param is true', () => {
+      const color = '#000';
+      const component = shallow(<AnimatedBackgroundColorView color={color} />);
+      const result = component.instance().getCurrentColor(true);
+
+      expect(result).toEqual(color);
+    });
+
+    it('should return mixed color when animationFinished param is false', () => {
+      const color = '#000';
+      const currentColor = '#fff';
+      const mixValue = 0.5;
+      const component = shallow(<AnimatedBackgroundColorView color={color} />);
+      component.instance().currentColor = currentColor;
+      const result = component.instance().getCurrentColor(false, mixValue);
+
+      expect(result).toEqual(
+        Color(currentColor)
+          .mix(Color(color), mixValue)
+          .hex()
+      );
     });
   });
 
